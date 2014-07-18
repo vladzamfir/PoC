01109ed89c517c5df77c5e54ffaaefb81d98ed5e
Vlad Zamfir <vlad@coinculture.info>
2014-07-18 13:01:39 +0200

    Added signature-tree infrastructure

--- PoC.go
+++ PoC.go
@@ -4,6 +4,7 @@ import (
         "fmt"
         "encoding/hex"
         "github.com/obscuren/sha3"
+	"github.com/obscuren/secp256k1-go"
   	//"io"
     	"io/ioutil"
 )
@@ -24,8 +25,7 @@ func read_data(file string) []byte {
 	return data
 }
 
-//makes the size of the data a multiple of chunk_size (in bytes)
-//returns the result
+//makes the size of the data a multiple of chunk_size (in bytes) by padding it on the right with zeros
 func pad_data(data []byte, chunk_size int) []byte  {
 	data_size := len(data)
 	remainder := data_size % chunk_size
@@ -61,16 +61,16 @@ func Sha3(data []byte) []byte {
 type Node struct {
 	value []byte
 	child []*Node
-	parent []*Node
+	parent []*Node //not in use yet, but will be used for producing proofs
 }
 
-/*this makes a modified merkle tree, one that doesn't require even powers of 2 number of slices
+/* this makes a modified merkle tree, one that doesn't require even powers of 2 number of slices
   for example, a merkle tree with 3 leaves would look like this:
        O
       / \
      O
     / \
-/*
+*/
 func merkle_tree(data_slice [][]byte) *Node {
 	num_chunks := len(data_slice) 
 	
@@ -94,6 +94,7 @@ func merkle_tree(data_slice [][]byte) *Node {
 			new_parent.value = append(new_parent.value, temp...)
 			new_parent.child = append(new_parent.child, orphans[2*i:2*i + 2]...)
 			orphans[i] = new_parent
+
 		}
 		if num_orphans % 2 == 1 {
 			orphans[num_orphans/2] = orphans[num_orphans - 1]
@@ -108,7 +109,16 @@ func Bytes2Hex(d []byte) string {
         return hex.EncodeToString(d)
 }
 
+func Hex2Bytes(str string) []byte {
+        h, _ := hex.DecodeString(str) //not handling errors
+        return h
+}
+
+
+//This recursively reports the decendants of a node
+//So far I've only used it to audit merkle trees
 func report_decendants(parent *Node, call_num *int) {
+	fmt.Println(len((*parent).child))
 	for i := range (*parent).child {
 		fmt.Printf("Call number: %d\n", *call_num)
 		fmt.Println(Bytes2Hex((*(*parent).child[i]).value))
@@ -118,17 +128,74 @@ func report_decendants(parent *Node, call_num *int) {
 	}
 }
 
-func main() {
-	
-	data := read_data("helloworld.txt")
-	fmt.Println(data)
-	padded := pad_data(data, 256)
-	fmt.Println(padded)
-	sliced := slice_data(padded, 16)
-	fmt.Println(sliced)
 
-	root := merkle_tree(sliced)
-	var call_count int = 0
-	report_decendants(root, &call_count) 
+func Signature(hash []byte, key []byte) []byte {
+	sig, _ := secp256k1.Sign(hash, key)
+	return sig
+}
+
+func sign_chunks(data_chunks [][]byte, key []byte) [][]byte {
+	num_slices := len(data_chunks)
+	sigs := make([][]byte, num_slices)
+	for i := range data_chunks {
+		sigs[i] = *new([]byte)
+		sigs[i] = append(sigs[i], Signature(data_chunks[i], key)...)
+	}
+	return sigs
+	
+}
+
+//not in use, yet
+func ECRecover(hash []byte, sig []byte) ([]byte, error) {
+	pubkey, err := secp256k1.RecoverPubkey(hash, sig)
+	if err != nil {
+		return nil, err
+	}
+	return pubkey, nil
+}
+
+func main() {
+	//Read data
+	data := read_data("helloworld.txt")
+	//fmt.Printf("Raw data:\n")
+	//fmt.Println(data)
+
+	chunk_size := 32 // in bytes
+
+	//Pad it so it's a multiple of the chunk_size
+	padded := pad_data(data, chunk_size)
+	//fmt.Printf("Padded data: \n")
+	//fmt.Println(padded)
+
+	//Slice the (padded) data into chunks
+	chunks := slice_data(padded, chunk_size)
+	//fmt.Printf("Sliced data, \n")
+	//fmt.Println(chunks)
+
+	fmt.Printf("Note that we have %d data chunks/signatures as the leaves of the merkle trees\n", len(chunks))
+
+	//Calculate the merkle root of the file
+	file_root := merkle_tree(chunks)
+	fmt.Printf("Merkle root of file: \n")
+	fmt.Println(Bytes2Hex((*file_root).value))
+
+	//This can be used to check the structure of the tree
+	//var calls int = 0
+	//report_decendants(file_root, &calls)
+
+	//A private key
+	key := Sha3(Hex2Bytes("hello world"))
+	fmt.Printf("Private key: \n")
+	fmt.Println(Bytes2Hex(key))
+
+	//Signatures of the data chunks
+	sigs := sign_chunks(chunks, key)
+	//fmt.Printf("Signatures: \n")
+	//fmt.Println(sigs)
+	
+	//Calculate merkle root of the signature
+	sig_root := merkle_tree(sigs)
+	fmt.Printf("Merkle root of sigs: \n")
+	fmt.Println(Bytes2Hex((*sig_root).value))
 	
 }
